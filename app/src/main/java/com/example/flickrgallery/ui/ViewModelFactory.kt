d6f7cf0a380fa3f319cf824bd012bd54621d29b0
Imanol Olveira <imanololveira@gmail.com>
2020-11-25 21:30:58 +0100

    Añadir ViewModel Factory común

---
+++ app/src/main/java/com/example/flickrgallery/ui/ViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.example.flickrgallery.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.flickrgallery.repo.LocalRepo
+
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory constructor(
+    private val localRepo: LocalRepo
+) : ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>) =
+        with(modelClass) {
+            when {
+                isAssignableFrom(MainViewModel::class.java) ->
+                    MainViewModel(localRepo)
+                else ->
+                    throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+            }
+        } as T
+}
+
