49f37eda218779a401501ea2ebe257b62f3ebe39
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2020-11-26 11:38:20 +0100

    Added onClickListener to the FAB so it shows a Toast. This way the user will be aware that the location was stored.

--- app/src/main/java/com/example/flickrgallery/ui/MainActivity.kt
+++ app/src/main/java/com/example/flickrgallery/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flickrgallery.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.flickrgallery.R
@@ -18,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setOnNavigationItemSelectedListener()
+        binding.storeLocationFab.setOnClickListener {
+            Toast.makeText(this, R.string.stored_location_success, Toast.LENGTH_LONG).show()
+            // TODO Store location with DAO
+        }
     }
 
     private fun setOnNavigationItemSelectedListener() {
