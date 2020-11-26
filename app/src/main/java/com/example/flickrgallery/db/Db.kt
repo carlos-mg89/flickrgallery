4ba14e17f3922beff7c82984434c2c03857d384e
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2020-11-26 12:22:20 +0100

    Added static variable into Db class so the instantiation of the database can be easily done and we don't need to know details of the database like the database file

--- app/src/main/java/com/example/flickrgallery/db/Db.kt
+++ app/src/main/java/com/example/flickrgallery/db/Db.kt
@@ -1,13 +1,29 @@
 package com.example.flickrgallery.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.flickrgallery.model.Photo
 
 
 @TypeConverters(DbConverters::class)
-@Database(entities = arrayOf(Photo::class), version = 1)
+@Database(entities = [Photo::class], version = 1)
 abstract class Db : RoomDatabase() {
     abstract fun photoDao(): PhotoDao
+
+    companion object {
+        private var INSTANCE: Db? = null
+        private const val DATABASE_NAME = "location-scout.db"
+
+        fun getDatabase(context: Context): Db {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                        context.applicationContext, Db::class.java, DATABASE_NAME
+                ).build()
+            }
+            return INSTANCE!!
+        }
+    }
 }
