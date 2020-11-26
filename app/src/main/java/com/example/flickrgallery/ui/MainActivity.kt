1cdccafc0c1927440eb6d9abcf96a25ef50cd51d
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2020-11-26 10:51:15 +0100

    Fully enabled the BottomNavigationView so initially loads the ExploreFragment. Right now when pressing into the other navigation items nothing will happen, but the code is already prepared to load a different fragment on each tab by using replaceFragmentContainerWith().

--- app/src/main/java/com/example/flickrgallery/ui/MainActivity.kt
+++ app/src/main/java/com/example/flickrgallery/ui/MainActivity.kt
@@ -2,15 +2,49 @@ package com.example.flickrgallery.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.flickrgallery.R
 import com.example.flickrgallery.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setOnNavigationItemSelectedListener()
+    }
+
+    private fun setOnNavigationItemSelectedListener() {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_explore -> {
+                    replaceFragmentContainerWith(ExploreFragment())
+                    true
+                }
+                R.id.nav_stored_locations -> {
+                    true
+                }
+                R.id.nav_favorite_photos -> {
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.selectedItemId = R.id.nav_explore
+    }
+
+    private fun replaceFragmentContainerWith(fragment: Fragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(binding.fragmentContainer.id, fragment)
+                .addToBackStack(null)
+                .commit()
     }
 }
