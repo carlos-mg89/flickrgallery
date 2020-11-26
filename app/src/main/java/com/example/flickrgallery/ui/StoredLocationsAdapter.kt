7868e2d697c924470dda4734666d90a887a6f9db
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2020-11-26 16:10:54 +0100

    Enabled interaction with Stored Locations list items, so we can delete them and we can get the photos around that location too

--- app/src/main/java/com/example/flickrgallery/ui/StoredLocationsAdapter.kt
+++ app/src/main/java/com/example/flickrgallery/ui/StoredLocationsAdapter.kt
@@ -8,11 +8,17 @@ import com.example.flickrgallery.model.StoredLocation
 import java.text.DateFormat
 import java.util.*
 
-class StoredLocationsAdapter(var storedLocations: List<StoredLocation>) :
+class StoredLocationsAdapter(
+        var storedLocations: List<StoredLocation>,
+        private val storedLocationSeeLocationPhotosClickListener: (StoredLocation) -> Unit,
+        private val storedLocationDeleteClickListener: (StoredLocation) -> Unit
+) :
     RecyclerView.Adapter<StoredLocationsAdapter.ViewHolder>() {
 
+    private lateinit var binding: StoredLocationsItemBinding
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = StoredLocationsItemBinding
+        binding = StoredLocationsItemBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
@@ -23,6 +29,14 @@ class StoredLocationsAdapter(var storedLocations: List<StoredLocation>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(storedLocations[position])
+        binding.seePhotosBtn.setOnClickListener {
+            storedLocationSeeLocationPhotosClickListener(storedLocations[position])
+        }
+        binding.deleteBtn.setOnClickListener {
+            storedLocationDeleteClickListener(storedLocations[position])
+            storedLocations = storedLocations.minus(storedLocations[position])
+            notifyDataSetChanged()
+        }
     }
 
     class ViewHolder(private val binding: StoredLocationsItemBinding) :
--- app/src/main/java/com/example/flickrgallery/ui/StoredLocationsFragment.kt
+++ app/src/main/java/com/example/flickrgallery/ui/StoredLocationsFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.example.flickrgallery.R
+import com.example.flickrgallery.client.FlickrApiClient
 import com.example.flickrgallery.databinding.StoredLocationsFragmentBinding
 import com.example.flickrgallery.db.Db
+import com.example.flickrgallery.model.StoredLocation
 import com.example.flickrgallery.repo.StoredLocationRepoImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,18 +20,26 @@ import kotlinx.coroutines.withContext
 class StoredLocationsFragment : Fragment() {
 
     private lateinit var binding: StoredLocationsFragmentBinding
+    private lateinit var storedLocationRepo: StoredLocationRepoImpl
+    private val storedLocationsAdapter = StoredLocationsAdapter(
+            emptyList(),
+            getPhotosFromLocationToDisplayThem(),
+            deleteStoredLocation()
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = StoredLocationsFragmentBinding.inflate(layoutInflater)
-
-        val storedLocationsAdapter = StoredLocationsAdapter(emptyList())
-        val database = Db.getDatabase(requireContext())
-        val storedLocationRepo = StoredLocationRepoImpl(database)
-
+        storedLocationRepo = StoredLocationRepoImpl(Db.getDatabase(requireContext()))
         binding.recyclerView.adapter = storedLocationsAdapter
 
+        loadStoredLocationsIntoAdapter()
+
+        return binding.root
+    }
+
+    private fun loadStoredLocationsIntoAdapter() {
         lifecycleScope.launch (Dispatchers.IO) {
             val storedLocations = storedLocationRepo.findAll()
             storedLocationsAdapter.storedLocations = storedLocations
@@ -36,8 +48,23 @@ class StoredLocationsFragment : Fragment() {
                 storedLocationsAdapter.setItems(storedLocations)
             }
         }
-
-        return binding.root
     }
 
+    private fun getPhotosFromLocationToDisplayThem(): (StoredLocation) -> Unit = {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val photosResult = FlickrApiClient.service.listPhotosNearLocation(it.latitude, it.longitude)
+            val locationPhotos = photosResult.photos.photo
+            // TODO Load location photos into the Explore Fragment and change the active tab to that one
+        }
+    }
+
+    private fun deleteStoredLocation(): (StoredLocation) -> Unit =
+            {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    storedLocationRepo.delete(it)
+                }
+                Toast.makeText(
+                        activity, R.string.stored_location_delete_success, Toast.LENGTH_LONG
+                ).show()
+            }
 }
