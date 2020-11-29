24c01c96480950430168bbda2a41221518be3911
Imanol Olveira <imanololveira@gmail.com>
2020-11-29 14:14:42 +0100

    Modificar MainActivity para que reciba posicines reales

--- app/src/main/java/com/example/flickrgallery/ui/MainActivity.kt
+++ app/src/main/java/com/example/flickrgallery/ui/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.flickrgallery.ui
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.flickrgallery.R
 import com.example.flickrgallery.databinding.ActivityMainBinding
 import com.example.flickrgallery.db.Db
+import com.example.flickrgallery.gps.GpsProvider
 import com.example.flickrgallery.model.StoredLocation
+import com.example.flickrgallery.repo.GpsRepo
+import com.example.flickrgallery.repo.GpsRepoImpl
 import com.example.flickrgallery.repo.StoredLocationRepoImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,12 +23,27 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var gpsRepo: GpsRepo
+
+    // TODO: 29/11/2020 Petición de permisos de forma simple
+    private val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            if (isGranted) {
+                storeLocation()
+            } else {
+                Toast.makeText(this, R.string.location_permission_denied, Toast.LENGTH_LONG).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val gpsProvider = GpsProvider(applicationContext)
+        gpsRepo = GpsRepoImpl(gpsProvider)
 
         setOnNavigationItemSelectedListener()
         setStoreLocationFabOnClickListener()
@@ -63,20 +83,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun setStoreLocationFabOnClickListener() {
         binding.storeLocationFab.setOnClickListener {
-            Toast.makeText(this, R.string.stored_location_success, Toast.LENGTH_LONG).show()
-            lifecycleScope.launch(Dispatchers.IO) {
-                val database = Db.getDatabase(applicationContext)
-                val storedLocationRepo = StoredLocationRepoImpl(database)
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
 
-                // TODO Get real location and ask user to enter a description for the new location
-                val newStoredLocation = StoredLocation().apply {
-                    latitude = 42.1115775
-                    longitude = 3.1304088
-                    description = "L'Escala"
-                }
+    private fun storeLocation() {
+        Toast.makeText(this, R.string.stored_location_success, Toast.LENGTH_LONG).show()
+        lifecycleScope.launch(Dispatchers.IO) {
+            val database = Db.getDatabase(applicationContext)
+            val storedLocationRepo = StoredLocationRepoImpl(database)
+            val snapshot = gpsRepo.getActualPosition()
 
-                storedLocationRepo.insert(newStoredLocation)
+            // TODO ask user to enter a description for the new location
+            val newStoredLocation = StoredLocation().apply {
+                latitude = snapshot.latitude
+                longitude = snapshot.longitude
+                description = "L'Escala"
             }
+
+            storedLocationRepo.insert(newStoredLocation)
         }
     }
 }
