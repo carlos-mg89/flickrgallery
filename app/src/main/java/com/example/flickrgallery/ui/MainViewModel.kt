72c95f632732782a6e085529696b05c0bbbab692
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2020-12-02 18:17:26 +0100

    Added photos LiveData so it can be observed by one of the ViewModel's consumers
    (cherry picked from commit 6d65a2b996f2daf3d715e5f379ffd8ab2603bbd5)

--- app/src/main/java/com/example/flickrgallery/ui/MainViewModel.kt
+++ app/src/main/java/com/example/flickrgallery/ui/MainViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.flickrgallery.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.flickrgallery.client.FlickrApiClient
+import com.example.flickrgallery.model.Photo
 import com.example.flickrgallery.repo.LocalRepo
 
 class MainViewModel(private val localRepo: LocalRepo) : ViewModel() {
+
+    private val _photos = MutableLiveData<List<Photo>>()
+    val photos: LiveData<List<Photo>>
+        get() = _photos
+
+    init {
+        _photos.value = ArrayList()
+    }
+
+    suspend fun setPhotosAt(latitude: Double, longitude: Double) {
+        val wayPointPhotosResult = FlickrApiClient.service.listPhotosNearLocation(latitude, longitude)
+        val wayPointPhotos = wayPointPhotosResult.photos.photo
+
+        localRepo.insertAllPhotos(wayPointPhotos)
+
+        this._photos.postValue(wayPointPhotos)
+    }
 }
