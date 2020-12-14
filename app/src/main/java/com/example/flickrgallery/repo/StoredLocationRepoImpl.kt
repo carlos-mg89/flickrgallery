01849f452c1e999bb5950f3c6bb6d72f45559871
Imanol Olveira <imanololveira@gmail.com>
2020-12-14 20:08:14 +0100

    flickrgallery-10 Añadir soporte para corrutinas  StoredLocationRepo

--- app/src/main/java/com/example/flickrgallery/repo/StoredLocationRepoImpl.kt
+++ app/src/main/java/com/example/flickrgallery/repo/StoredLocationRepoImpl.kt
@@ -5,21 +5,19 @@ import com.example.flickrgallery.model.StoredLocation
 
 class StoredLocationRepoImpl (private val database: Db) : StoredLocationRepo {
 
-    override fun findAll(): List<StoredLocation> {
+    override suspend fun findAll(): List<StoredLocation> {
         return database.storedLocationDao().findAll()
     }
 
-    override fun insert(storedLocation: StoredLocation) {
+    override suspend fun insert(storedLocation: StoredLocation) {
         database.storedLocationDao().insert(storedLocation)
     }
 
-    override fun update(storedLocation: StoredLocation) {
+    override suspend fun update(storedLocation: StoredLocation) {
         database.storedLocationDao().update(storedLocation)
     }
 
-    override fun delete(storedLocation: StoredLocation) {
+    override suspend fun delete(storedLocation: StoredLocation) {
         database.storedLocationDao().delete(storedLocation)
     }
-
-
 }
