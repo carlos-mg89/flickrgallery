6a762be1bb1ddf1c5f960a7ce17492b8e643e297
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2020-12-16 10:46:42 +0100

    Refactor StoredLocationsFragment to use it's own ViewModel instead of directly accessing DAOs

--- app/src/main/java/com/example/flickrgallery/repo/StoredLocationRepo.kt
+++ app/src/main/java/com/example/flickrgallery/repo/StoredLocationRepo.kt
@@ -1,9 +1,11 @@
 package com.example.flickrgallery.repo
 
+import androidx.lifecycle.LiveData
 import com.example.flickrgallery.model.StoredLocation
 
 interface StoredLocationRepo {
-    suspend fun findAll(): List<StoredLocation>
+    fun getAllLiveData(): LiveData<List<StoredLocation>>
+    suspend fun getAll(): List<StoredLocation>
     suspend fun insert(storedLocation: StoredLocation)
     suspend fun update(storedLocation: StoredLocation)
     suspend fun delete(storedLocation: StoredLocation)
