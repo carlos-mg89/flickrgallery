f2afe772ebb9ed175833b541425a6ab73bfb14a3
Carlos Alberto Martínez Gadea <carlosmartinezgadea@gmail.com>
2020-12-22 12:20:07 +0100

    Refactor on PhotoDetails fragment, to remove empty function and separate the startup of the ViewModel

--- app/src/main/java/com/example/flickrgallery/ui/PhotoDetailsFragment.kt
+++ app/src/main/java/com/example/flickrgallery/ui/PhotoDetailsFragment.kt
@@ -1,12 +1,13 @@
 package com.example.flickrgallery.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import com.bumptech.glide.Glide
 import com.example.flickrgallery.R
 import com.example.flickrgallery.databinding.PhotoDetailsFragmentBinding
@@ -26,57 +27,44 @@ class PhotoDetailsFragment : Fragment() {
     private lateinit var binding: PhotoDetailsFragmentBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View {
-
         binding = PhotoDetailsFragmentBinding.inflate(inflater)
-        photo = arguments?.getParcelable<Photo>(EXTRA_PHOTO)!!
+        photo = arguments?.getParcelable(EXTRA_PHOTO)!!
         Glide.with(this).load(photo.getMedium640Url()).into(binding.photo)
         binding.saveDataText.text = photo.savedDate.toString()
         binding.descriptionText.text = photo.title
-        binding.commentsText.text = obtainCommentsPhoto().toString()
         binding.saveImageButton.setOnClickListener {
             viewModel.toggleSaveStatus(photo)
         }
-
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViewModel()
+        subscribeUi()
+        viewModel.getPhotoInitialState(photo)
+    }
+
+    private fun initViewModel() {
         val database = Db.getDatabase(requireContext().applicationContext)
         val photoRepo = PhotoRepoImpl(database)
         val factory = PhotoDetailsViewModelFactory(photoRepo)
-        viewModel = ViewModelProvider(this, factory).get(PhotoDetailsViewModel::class.java)
-        subscribeUi()
-        viewModel.getPhotoInitialState(photo)
-
-
+        viewModel = ViewModelProvider(this, factory).get()
     }
-
-    private fun obtainCommentsPhoto() {
-        //TODO: Get comments from api
-    }
-
 
     private fun subscribeUi() {
-
         viewModel.favoriteStatus.observe(this.viewLifecycleOwner) { isSaved ->
             val drawable = if (isSaved) {
-
                 ContextCompat.getDrawable(requireContext(), R.drawable.photo_saved)
-
             } else {
-
                 ContextCompat.getDrawable(requireContext(), R.drawable.photo_no_saved)
-
             }
             binding.saveImageButton.setImageDrawable(drawable)
-
         }
     }
-
 
 }
