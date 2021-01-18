8c5ba7225f0421d2cdc87b3771e6a93509f3a828
Imanol Olveira <imanololveira@gmail.com>
2021-01-18 18:34:29 +0100

    flickrgallery-43 Migrar StoredLocationsFragment a DataBinding
    Formatear la fecha.

--- app/src/main/java/com/example/flickrgallery/model/StoredLocation.kt
+++ app/src/main/java/com/example/flickrgallery/model/StoredLocation.kt
@@ -5,6 +5,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
 import java.util.*
 
 @Entity(tableName = "stored_locations")
@@ -17,7 +18,18 @@ class StoredLocation : Parcelable {
     @ColumnInfo(name = "saved_date")
     var savedDate: Date = Calendar.getInstance().time
 
+    val savedDateString: String
+        get() = stringFromSavedDate()
+
     var latitude: Double = 0.0
     var longitude: Double = 0.0
     var description: String = ""
+
+
+    private fun stringFromSavedDate(): String {
+        val dateFormat = DateFormat.getDateTimeInstance(
+            DateFormat.SHORT, DateFormat.SHORT, Locale.getDefault()
+        )
+        return try { dateFormat.format(savedDate) } catch (e: Exception){ "" }
+    }
 }
