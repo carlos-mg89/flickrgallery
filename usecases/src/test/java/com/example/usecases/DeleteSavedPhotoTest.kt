a517d3c11424351a89838a14ece1337cc6315002
Arman <arman.agora@gmail.com>
2021-02-18 11:32:51 +0100

    Added DeletedSavedPhotoTest

---
+++ usecases/src/test/java/com/example/usecases/DeleteSavedPhotoTest.kt
@@ -0,0 +1,40 @@
+package com.example.usecases
+
+import com.example.data.repo.PhotosRepo
+import com.example.domain.Photo
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class DeleteSavedPhotoTest {
+    @Mock
+    lateinit var deleteSavedPhoto: DeleteSavedPhoto
+
+    @Mock
+    lateinit var photosRepo: PhotosRepo
+
+    @Before
+    fun setUp() {
+        deleteSavedPhoto = DeleteSavedPhoto(photosRepo)
+    }
+
+
+    @Test
+    fun `DeleteSavedPhoto calls `() {
+        runBlocking {
+            val photo = Photo().apply {
+                id = "1"
+                title = "Title test"
+                isSaved = true
+            }
+            deleteSavedPhoto.invoke(photo)
+
+            verify(photosRepo).deleteSavedPhoto(photo)
+        }
+    }
+}
